4606bc8e0996252cddb0e0137bbeb976f7455cda
simao.bronze <simao.bronze@onesource.pt>
2025-05-30 16:28:22 +0100

    ultimo update do projeto

--- Empresa_V2/Assets/Scripts/Responses/LoginResponse.cs
+++
@@ -1,6 +0,0 @@
-[System.Serializable]
-public class LoginResponse
-{
-    public User user;
-}
-
--- Empresa_V2/Assets/Scripts/WebRTC/VideoroomPlugin.cs
+++
@@ -1,8 +0,0 @@
-﻿public class VideoroomPlugin : JanusPlugin
-{
-    public VideoroomPlugin(long id)
-    {
-        this.id = id;
-        this.name = "janus.plugin.videoroom";
-    }
-}
